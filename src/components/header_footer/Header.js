441030b3d8c2dd072a7469e31233c6c25a4cca26
Alan Ayoub <alanayoub@me.com>
2020-03-08 16:50:08 -0700

    slider with images working

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -11,8 +11,26 @@ import IconButton from '@material-ui/core/IconButton';
 class Header extends Component {
 
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
     }
+
+    componentDidMount(){
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    handleScroll = () => {
+        if(window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+        } else {
+            this.setState({
+                headerShow: false
+            })
+        }
+    }
+
 
     toggleDrawer = (value) => {
         this.setState({
@@ -25,7 +43,7 @@ class Header extends Component {
                 <AppBar
                     position="fixed"
                     style={{
-                        backgroundColor: '#2f2f2f',
+                        backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                         boxShadow: 'none',
                         padding: '10px 0px'
                     }}
