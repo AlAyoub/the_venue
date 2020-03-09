c5ad23bcbc86bf8de706b201a9a02e70cc5ff0ca
Alan Ayoub <alanayoub@me.com>
2020-03-08 19:58:20 -0700

    countdown almost done

---
+++ src/components/featured/TimeUntil.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class TimeUntil extends Component {
+    
+    state = {
+        
+    }
+    
+    render() {
+        return (
+            <div className="countdown_wrapper">
+                <div className="countdown_top">
+                    Event starts in
+                </div>
+                <div className="countdown_bottom">
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            23
+                        </div>
+                        <div className="countdown_tag">
+                            Days
+                        </div>
+                    </div>
+                    <div className="countdown_item">
+                        <div className="countdown_time">
+                            2
+                        </div>
+                        <div className="countdown_tag">
+                            Hs
+                        </div>
+                    </div><div className="countdown_item">
+                        <div className="countdown_time">
+                            20
+                        </div>
+                        <div className="countdown_tag">
+                            Min
+                        </div>
+                    </div><div className="countdown_item">
+                        <div className="countdown_time">
+                            35
+                        </div>
+                        <div className="countdown_tag">
+                            Sec
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TimeUntil;
