9fa63155899dcbe8cc387e4a631d6155153486b9
Alan Ayoub <alanayoub@me.com>
2020-03-11 06:46:48 -0700

    highlights section 30% complete

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueInfo from './components/venueinfo';
+import Highlights from './components/highlights';
 
 function App() {
   return (
-    <div className="App" style={{height:"1500px", background:"cornflowerblue" }}>
+    <div className="App" style={{height:"1500px"}}>
       <Header/>
       <Featured/>
       <VenueInfo/>
+      <Highlights/>
+
     </div>
   );
 }
